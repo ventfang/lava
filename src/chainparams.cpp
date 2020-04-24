c8aed153ce2a77dac901bec70e0f50573131e55c
vent.fang <vent.fang@gmail.com>
2020-04-24 09:48:59 +0800

    enable fallback fee

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -168,7 +168,7 @@ public:
         };
 
         /* disable fallback fee on mainnet */
-        m_fallback_fee_enabled = false;
+        m_fallback_fee_enabled = true;
 
         nSlotLength = 2048;
     }
